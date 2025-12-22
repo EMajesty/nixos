16f3c98bacf158fa5ec2ab3b7330bf03c6374d35
EMajesty <lassi.laitinen@protonmail.com>
2025-12-22 14:58:48 +0200

    good shit

--- rebuild_laptop.sh
+++ rebuild_laptop.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -euo pipefail
+git add .
+git commit -m "good shit"
+git push
 sudo nix flake update
 sudo nixos-rebuild switch --flake /home/emaj/nixos#laptop
 sudo nix-env --profile "/nix/var/nix/profiles/system" --delete-generations "14d"
