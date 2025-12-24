5774c15d5fd27e4901a9cd0338b2f92da971818b
EMajesty <lassi.laitinen@protonmail.com>
2025-12-24 20:28:53 +0200

    good shit

--- flake.nix
+++ flake.nix
@@ -7,52 +7,52 @@
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nvf.url = "github:notashelf/nvf";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-unstable,
-    home-manager,
-    nvf,
-    ...
-  }: let
-    system = "x86_64-linux";
-    commonModules = [
-      ./modules/common.nix
-      home-manager.nixosModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.emaj = {
-		imports = [
-			./home.nix
-		      nvf.homeManagerModules.default
-	      ];
-	      };
-      }
-    ];
-  in {
-    nixosConfigurations = {
-      laptop = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {inherit self;};
-        modules =
-          commonModules
-          ++ [
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      home-manager,
+      nvf,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      commonModules = [
+        ./modules/common.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.emaj = {
+            imports = [
+              ./home.nix
+              nvf.homeManagerModules.default
+            ];
+          };
+        }
+      ];
+    in
+    {
+      nixosConfigurations = {
+        laptop = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit self; };
+          modules = commonModules ++ [
             ./hosts/laptop.nix
           ];
-      };
+        };
 
-      desktop = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {inherit self;};
-        modules =
-          commonModules
-          ++ [
+        desktop = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit self; };
+          modules = commonModules ++ [
             ./hosts/desktop.nix
           ];
+        };
       };
     };
-  };
 }
