bd8252d2b27f7ecf24388a677cf3b5ecbe6c997d
EMajesty <lassi.laitinen@protonmail.com>
2025-12-24 12:25:36 +0200

    good shit

--- modules/nvf-configuration.nix
+++ modules/nvf-configuration.nix
@@ -6,41 +6,43 @@
 programs.nvf = {
 enable = true;
 settings = {
-        vim.viAlias = true;
-        vim.vimAlias = true;
-        vim.theme.enable = true;
-        vim.theme.name = "gruvbox";
-        vim.theme.style = "dark";
+        vim = {
+                viAlias = true;
+                vimAlias = true;
+                theme.enable = true;
+                theme.name = "gruvbox";
+                theme.style = "dark";
 
-        vim.lsp.enable = true;
-        vim.lsp.formatOnSave = true;
-        vim.treesitter.enable = true;
+                lsp.enable = true;
+                lsp.formatOnSave = true;
+                treesitter.enable = true;
 
-        vim.statusline.lualine.enable = true;
-        vim.telescope.enable = true;
-        vim.autocomplete.nvim-cmp.enable = true;
+                statusline.lualine.enable = true;
+                telescope.enable = true;
+                autocomplete.nvim-cmp.enable = true;
 
-        vim.globals.mapleader = " ";
-        vim.keymaps = [
-                {
-                        key = "<leader>pv";
-                        mode = "n";
-                        silent = true;
-                        action = "<cmd>Ex<CR>";
-                }
-                {
-                        key = "<leader>m";
-                        mode = ["n" "x"];
-                        silent = true;
-                        action = ":make<CR>";
-                }
-                {
-                        key = "<leader>y";
-                        mode = ["n" "v"];
-                        silent = true;
-                        action = "<cmd>\"+y";
-                }
-        ];
+                globals.mapleader = " ";
+                keymaps = [
+                        {
+                                key = "<leader>pv";
+                                mode = "n";
+                                silent = true;
+                                action = "<cmd>Ex<CR>";
+                        }
+                        {
+                                key = "<leader>m";
+                                mode = ["n" "x"];
+                                silent = true;
+                                action = ":make<CR>";
+                        }
+                        {
+                                key = "<leader>y";
+                                mode = ["n" "v"];
+                                silent = true;
+                                action = "<cmd>\"+y";
+                        }
+                ];
+        };
 };
 };
 }
