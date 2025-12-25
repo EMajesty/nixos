508ef10a4f748f45005f29fdf284693675d7ea0c
EMajesty <lassi.laitinen@protonmail.com>
2025-12-25 15:29:09 +0200

    good shit

--- modules/common.nix
+++ modules/common.nix
@@ -90,6 +90,7 @@
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+    image = "https://i.redd.it/7oxaztid8wud1.png";
     polarity = "dark";
   };
 
