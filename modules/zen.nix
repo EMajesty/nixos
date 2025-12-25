01d06f20d4134bb727e198beb8d03f829b8ae079
EMajesty <lassi.laitinen@protonmail.com>
2025-12-25 13:12:11 +0200

    good shit

--- modules/zen.nix
+++ modules/zen.nix
@@ -25,9 +25,45 @@
       let
         pins = {
           "perplexity" = {
+            id = "9d8a8f91-7e29-4688-ae2e-da4e49d4a179";
             url = "https://perplexity.ai";
             isEssential = true;
             position = 101;
+          };
+          "github" = {
+            url = "https://github.com";
+            isEssential = true;
+            position = 102;
+          };
+          "annas" = {
+            url = "https://annas-archive.org";
+            isEssential = true;
+            position = 103;
+          };
+          "proton" = {
+            url = "https://mail.proton.me";
+            isEssential = true;
+            position = 104;
+          };
+          "onshape" = {
+            url = "https://cad.onshape.com/signin";
+            isEssential = true;
+            position = 201;
+          };
+          "fluidd" = {
+            url = "http://192.168.1.130/#/";
+            isEssential = true;
+            position = 202;
+          };
+          "pedal" = {
+            url = "pedalplayground.com";
+            isEssential = true;
+            position = 203;
+          };
+          "pinterest" = {
+            url = "https://pinterest.com";
+            isEssential = true;
+            position = 204;
           };
         };
       in
