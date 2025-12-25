18d40c593c68c8777c35b701734bf0d07a592cc8
EMajesty <lassi.laitinen@protonmail.com>
2025-12-25 13:27:23 +0200

    good shit

--- home.nix
+++ home.nix
@@ -20,10 +20,36 @@
     ./modules/zsh.nix
     ./modules/misc.nix
     ./modules/nvf-configuration.nix
-    ./modules/zen.nix
+    # ./modules/zen.nix
     ./modules/ghostty.nix
   ];
 
   programs.home-manager.enable = true;
   home.stateVersion = "25.11";
+
+  programs.zen-browser = {
+    enable = true;
+    policies = {
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
+    profiles.default.search = {
+      force = true;
+      default = "ddg";
+    };
+  };
 }
