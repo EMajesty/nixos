b8f47651d07839965f2a5890c1486f807e1a5efe
EMajesty <lassi.laitinen@protonmail.com>
2025-12-25 11:08:34 +0200

    good shit

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -3,11 +3,11 @@
 {
   programs.ghostty = {
     enable = true;
-    # settings = {
-    #   theme = "Dracula";
-    #   background-opacity = "0.8";
-    #   background-blur = true;
-    #   confirm-close-surface = false;
-    # };
+    settings = {
+      #   theme = "Dracula";
+      #   background-opacity = "0.8";
+      #   background-blur = true;
+      #   confirm-close-surface = false;
+    };
   };
 }
