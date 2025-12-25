110af05ab7afc3094af4b26191f3bbe525c0b1c6
EMajesty <lassi.laitinen@protonmail.com>
2025-12-25 14:39:21 +0200

    good shit

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -2,9 +2,4 @@
 {
   stylix.enable = true;
   stylix.image = ../../Pictures/wallpapers/wallpaper.png;
-  stylix.fonts = {
-    serif = config.stylix.fonts.monospace;
-    sansSerif = config.stylix.fonts.monospace;
-    emoji = config.stylix.fonts.monospace;
-  };
 }
