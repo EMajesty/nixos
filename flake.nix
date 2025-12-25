1814a4002926b1b934127c1d487faa0d89072cae
EMajesty <lassi.laitinen@protonmail.com>
2025-12-25 14:25:31 +0200

    good shit

--- flake.nix
+++ flake.nix
@@ -30,6 +30,9 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.emaj = {
+            nixpkgs.config = {
+              allowUnfree = true;
+            };
             imports = [
               ./home.nix
               nvf.homeManagerModules.default
