4b46b261e42f7c1c22a7a1e628ca7d90d7cf5386
EMajesty <lassi.laitinen@protonmail.com>
2025-12-25 15:33:08 +0200

    good shit

--- modules/common.nix
+++ modules/common.nix
@@ -92,6 +92,7 @@
     base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
     image = pkgs.fetchurl {
       url = "https://i.redd.it/7oxaztid8wud1.png";
+      hash = "sha256-OVmNYW4QzGnzIc7+H8Hdxso10LlRbzgnkfjxDa42VZs=";
     };
     polarity = "dark";
   };
