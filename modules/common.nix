f4534f58594bb6d4a569e6869470fa0840135f36
EMajesty <lassi.laitinen@protonmail.com>
2026-01-01 18:13:31 +0200

    good shit

--- modules/common.nix
+++ modules/common.nix
@@ -127,7 +127,7 @@
   };
 
   programs.hyprland.enable = true;
-  programs.hyprpaper.enable = true;
+  services.hyprpaper.enable = true;
   services.hypridle.enable = true;
   programs.hyprlock.enable = true;
 
