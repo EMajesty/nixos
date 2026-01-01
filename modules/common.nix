8741e00a87276319e9c5232f7a2838dd71ecbcb1
EMajesty <lassi.laitinen@protonmail.com>
2026-01-01 17:42:42 +0200

    good shit

--- modules/common.nix
+++ modules/common.nix
@@ -108,7 +108,7 @@
       # nvf.enable = false;
     };
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/evergreen.yaml";
     image = pkgs.fetchurl {
       url = "https://i.redd.it/7oxaztid8wud1.png";
       hash = "sha256-OVmNYW4QzGnzIc7+H8Hdxso10LlRbzgnkfjxDa42VZs=";
