721035b6ce57fbf3c00a1238e956bc43de1c4969
EMajesty <lassi.laitinen@protonmail.com>
2026-01-01 17:50:36 +0200

    good shit

--- rebuild_laptop.sh
+++ rebuild_laptop.sh
@@ -3,7 +3,5 @@
 git add .
 git commit -m "good shit"
 git push
-sudo nix flake update
 sudo nixos-rebuild switch --flake /home/emaj/nixos#laptop
-sudo nix-collect-garbage --delete-older-than 30d
 # sudo nixos-rebuild boot --flake /home/emaj/nixos#laptop
