fd3ced53be5156813d96ddca6ef7510ff7f883a6
EMajesty <lassi.laitinen@protonmail.com>
2026-01-01 18:03:52 +0200

    good shit

--- modules/common.nix
+++ modules/common.nix
@@ -109,7 +109,7 @@
     };
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/evergreen.yaml";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/horizon-dark.yaml";
     image = pkgs.fetchurl {
       url = "https://i.redd.it/7oxaztid8wud1.png";
       hash = "sha256-OVmNYW4QzGnzIc7+H8Hdxso10LlRbzgnkfjxDa42VZs=";
