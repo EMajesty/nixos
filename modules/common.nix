74e804e7e3a7e1e8be26ba714bed169a4ffcb96f
EMajesty <lassi.laitinen@protonmail.com>
2026-01-09 23:47:07 +0200

    good shit

--- modules/common.nix
+++ modules/common.nix
@@ -137,8 +137,9 @@
       #     transparentBackground.main = true;
       # };
     };
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/zenburn.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/evergreen.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/penumbra-dark.yaml";
     image = pkgs.fetchurl {
