aac04fdb3577daf339fa8cbefd2fb1c2bc71395a
EMajesty <lassi.laitinen@protonmail.com>
2026-01-12 22:23:24 +0200

    good shit

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -6,6 +6,7 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+
     shellAliases = {
       gs = "git status";
       ls = "eza -lh --group-directories-first --icons=auto";
@@ -13,6 +14,16 @@
       lt = "eza --tree --level=2 --long --icons --git";
       lta = "lt -a";
     };
+
+    completionInit = ''
+      autoload -Uz compinit
+      compinit
+    '';
+    initExtra = ''
+      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+      zstyle ':completion:*' menu select
+    '';
+
   };
   programs.oh-my-posh = {
     enable = true;
